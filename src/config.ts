0140b628c30806da945a6830db2453b71e4d7765
Toni Haila <toni.haila@edu.turkuamk.fi>
2025-09-15 22:01:45 +0300

    Prettier formatting

--- src/config.ts
+++ src/config.ts
@@ -1,30 +1,30 @@
-import fs from "fs"
-import os from "os"
-import path from "path"
+import fs from "fs";
+import os from "os";
+import path from "path";
 
-const cfgFile = ".gatorconfig.json"
-const cfgPath = path.join(os.homedir(), cfgFile)
+const cfgFile = ".gatorconfig.json";
+const cfgPath = path.join(os.homedir(), cfgFile);
 
 type Config = {
-    dbUrl: string,
-    currentUserName: string
-}
+  dbUrl: string;
+  currentUserName: string;
+};
 
 export function setUser(user: string) {
-    const config = readConfig()
-    config.currentUserName = user
-    fs.writeFileSync(cfgPath, JSON.stringify(config))
+  const config = readConfig();
+  config.currentUserName = user;
+  fs.writeFileSync(cfgPath, JSON.stringify(config));
 }
 
 export function readConfig(): Config {
-    let config = fs.readFileSync(cfgPath, { encoding: "utf-8" })
-    return validateConfig(JSON.parse(config))
+  let config = fs.readFileSync(cfgPath, { encoding: "utf-8" });
+  return validateConfig(JSON.parse(config));
 }
 
 function validateConfig(rawConfig: any): Config {
-    const cfg: Config = rawConfig
-    if (typeof cfg.dbUrl !== "string" || typeof cfg.currentUserName !== "string") {
-        throw new Error("Config validation failed");
-    }
-    return cfg
+  const cfg: Config = rawConfig;
+  if (typeof cfg.dbUrl !== "string" || typeof cfg.currentUserName !== "string") {
+    throw new Error("Config validation failed");
+  }
+  return cfg;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { setUser, readConfig } from "src/config";
 
 function main() {
-    let user = "Toni"
-    setUser(user)
-    console.log(readConfig())
+  let user = "Toni";
+  setUser(user);
+  console.log(readConfig());
 }
 
 main();
